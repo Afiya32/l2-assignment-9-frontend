35bd40969b0a28ec1f5bb7fb30c580f4faedc978
Afiya32 <biplobguru123@gmail.com>
2024-12-10 10:21:59 +0600

    frontend setup completed

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <button className='btn btn-primary'>click here</button>
+        <h1 className='text-red-200'>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
